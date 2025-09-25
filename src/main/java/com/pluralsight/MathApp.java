a8cb9fea9c55b107b26a1131011b1eb2cce5a932
hdyess <henrypdyess@gmail.com>
2025-09-25 13:02:37 -0400

    inital

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,23 +1,147 @@
 package com.pluralsight;
+import java.lang.Math;
 
 public class MathApp {
     public static void main(String[] args){
 
         // Question 1:
+
         // declare variables here
+        int garySalary = 75_000;
+        int bobSalary = 77_000;
+        int highestSalary = Math.max(garySalary, bobSalary);
+
         // then code solution
+        // n/a
+
         // then use System.out.println() to display results
+        System.out.println("The highest salary is... " + highestSalary + "!");
+
         // ex: System.out.println("The answer is " + answer);
 
         // REPEAT FOR NEXT EXERCISE
+
 
         // Question 2:
+
         // declare variables here
+        double carPrice  = 15_000;
+        double truckPrice = 20_000;
+
         // then code solution
+        // n/a
+
         // then use System.out.println() to display results
+        System.out.println("The cheaper vehicle is priced at: " + Math.min(carPrice, truckPrice));
+
+
         // ex: System.out.println("The answer is " + answer);
 
+
         // REPEAT FOR NEXT EXERCISE
+
+
+        // Question 3:
+
+        // declare variables here
+
+
+        // then code solution
+
+
+        // then use System.out.println() to display results
+
+
+        // ex: System.out.println("The answer is " + answer);
+
+
+        // REPEAT FOR NEXT EXERCISE
+
+
+        // Question 4:
+
+        // declare variables here
+
+
+        // then code solution
+
+
+        // then use System.out.println() to display results
+
+
+        // ex: System.out.println("The answer is " + answer);
+
+
+        // REPEAT FOR NEXT EXERCISE
+
+
+        // Question 5:
+
+        // declare variables here
+
+
+        // then code solution
+
+
+        // then use System.out.println() to display results
+
+
+        // ex: System.out.println("The answer is " + answer);
+
+
+        // REPEAT FOR NEXT EXERCISE
+
+
+        // Question 6:
+
+        // declare variables here
+
+
+        // then code solution
+
+
+        // then use System.out.println() to display results
+
+
+        // ex: System.out.println("The answer is " + answer);
+
+
+        // REPEAT FOR NEXT EXERCISE
+
+
+        // Question 7:
+
+        // declare variables here
+
+
+        // then code solution
+
+
+        // then use System.out.println() to display results
+
+
+        // ex: System.out.println("The answer is " + answer);
+
+
+        // REPEAT FOR NEXT EXERCISE
+
+
+        // Question 8:
+
+        // declare variables here
+
+
+        // then code solution
+
+
+        // then use System.out.println() to display results
+
+
+        // ex: System.out.println("The answer is " + answer);
+
+
+        // REPEAT FOR NEXT EXERCISE
+
 
     }
 }
