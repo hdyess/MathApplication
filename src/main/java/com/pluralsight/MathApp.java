7ec0dc7a4cde6b7ce60926e3ec8ff5fb91fd4154
hdyess <henrypdyess@gmail.com>
2025-09-25 12:02:47 -0400

    inital

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args){
+
+        // Question 1:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+
+        // REPEAT FOR NEXT EXERCISE
+
+        // Question 2:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+
+        // REPEAT FOR NEXT EXERCISE
+
+    }
+}
